48fc6d9d0cc7d96bbd76fb1280227b04f7a0cd9e
Aniket Rajnish <aniket.r@iitgn.ac.in>
2022-01-12 11:15:24 +0530

    Initial commit

---
+++ Assets/Scripts/FourBarMechanism.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FourBarMechanism : MonoBehaviour
+{
+    [SerializeField] float forceFactor, dir;   
+
+    private void FixedUpdate()
+    {
+        FourBarForce();
+    }    
+
+    void FourBarForce()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForceAtPosition(rb.mass * transform.up * forceFactor, transform.TransformPoint(Vector3.right * dir));
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 4 bar mechanism unity implementation
+ 4 bar mechanism implemented in Unity
